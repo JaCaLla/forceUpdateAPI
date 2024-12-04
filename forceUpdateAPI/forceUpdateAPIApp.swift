f84690ba8821141d3762a9a53fc196bdb34a8db2
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-04 21:30:28 +0100

    iOS blank project created

---
+++ forceUpdateAPI/forceUpdateAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  forceUpdateAPIApp.swift
+//  forceUpdateAPI
+//
+//  Created by Javier Calatrava on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct forceUpdateAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ forceUpdateAPITests/forceUpdateAPITests.swift
@@ -0,0 +1,17 @@
+//
+//  forceUpdateAPITests.swift
+//  forceUpdateAPITests
+//
+//  Created by Javier Calatrava on 4/12/24.
+//
+
+import Testing
+@testable import forceUpdateAPI
+
+struct forceUpdateAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
